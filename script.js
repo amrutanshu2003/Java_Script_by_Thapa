75164b3394ae604fe7b70e0e1c17689cc444ed95
Amrutanshu Panda <amrutanshu2003@gmail.com>
2026-02-10 10:11:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert("Hello World");
+console.log("Hello World");
+//1.Values and Variables in JavaScript
+var my_age = 45;
+console.log("my age is: "+my_age);
+//a.varialbe name start with letter, _ and $.
+var _myname = "Somu";
+console.log("my name is: "+_myname);
+var 
