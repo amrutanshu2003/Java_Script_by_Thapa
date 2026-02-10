1f033fdc9e48a9fc193180337118f72a2103ed36
Amrutanshu Panda <amrutanshu2003@gmail.com>
2026-02-10 16:51:58 +0530

    concat&coercion

---
+++ concat_&coercion.js
@@ -0,0 +1,17 @@
+console.log("somu"+10);
+console.log("Hello "+ "world");
+console.log(10+"20");
+console.log("10"+20);
+console.log(10+20);
+console.log(9-"5");
+console.log("9"-5);
+console.log("Java"+ "Script");
+console.log(""+"");
+console.log(""+0);
+console.log("Amru"-"Panda" );//NaN because "Amru" and "Panda" cannot be converted to numbers
+console.log(true+true);//2 because true is converted to 1 and 1+1=2
+console.log(true+false);//1 because true is converted to 1 and false is converted to 0 and 1+0=1
+console.log(false+false);//0 because false is converted to 0 and 0+0=0
+console.log(false+true);//1 because false is converted to 0 and true is converted to 1 and 0+1=1
+console.log(false-true);//-1 because false is converted to 0 and true is converted to 1 and 0-1=-1
+console.log(`this is :${true-false}`);
